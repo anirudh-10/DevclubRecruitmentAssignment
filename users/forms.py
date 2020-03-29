c7dee907fb822fbcb09e9d3f67c89dd4691d7b3b
anirudh-10 <59475119+anirudh-10@users.noreply.github.com>
2020-03-29 06:27:19 +0530

    Update forms.py

--- users/forms.py
+++ users/forms.py
@@ -14,12 +14,12 @@ class UserRegisterForm(UserCreationForm):
         username = self.cleaned_data.get('username')
         if email and User.objects.filter(email=email).count() > 0:
             raise forms.ValidationError(u'This email address is already registered.')
-        elif "@gmail.com" not in email:   
+        elif "@iitd.ac.in" not in email:   
             raise forms.ValidationError("Register with IITD IDs with domain only '@iitd.ac.in'") 
-        elif xxx==False:
-                raise forms.ValidationError("Invalid email")
-        elif(xxx==None):
-            raise forms.ValidationError("Invalid Email Address")        
+        #elif xxx==False:
+         #       raise forms.ValidationError("Invalid email")
+        #elif(xxx==None):
+         #   raise forms.ValidationError("Invalid Email Address")        
         return email    
         
 
